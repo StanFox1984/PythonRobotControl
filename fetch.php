daa4aa1ab908eecf5eabc91d41ebf4bc7d461423
Stanislav Lisovskiy <Stanislav.Lisovskiy@espotel.com>
2014-03-23 00:19:20 +0200

    more files..

---
+++ fetch.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$host = $_GET['argument1']; 
+$user = $_GET['argument2']; 
+$pass = $_GET['argument3'];; 
+
+
+$r = mysql_connect($host, $user, $pass);
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Connection established\n"; 
+}
+
+$query = "SELECT * from test";
+
+$rs = mysql_query($query);
+
+if (!$rs) {
+    echo "Could not execute query: $query\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Query: $query executed\n"; 
+}
+
+$row = mysql_fetch_row($rs);
+
+echo "$row[0]\n";
+
+mysql_close();
+
+?>
+<?php
+
