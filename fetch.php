1efeb302e05b5d10289b8417cab7c83e113d9be9
Stanislav Lisovskiy <Stanislav.Lisovskiy@espotel.com>
2014-03-23 11:48:27 +0200

    fetching rows

--- fetch.php
+++ fetch.php
@@ -13,7 +13,7 @@ if (!$r) {
     echo "Could not connect to server\n";
     trigger_error(mysql_error(), E_USER_ERROR);
 } else {
-    echo "Connection established\n"; 
+    //echo "Connection established\n"; 
 }
 
 $r = mysql_select_db('testst1984');
@@ -24,11 +24,11 @@ if( !$r)
 }
 else
 {
-  echo "database selected";
+  //echo "database selected";
 }
 
 
-$query = "SELECT id, name from test";
+$query = "SELECT id, latitude, longitude, devname from bluetoothmap";
 
 $rs = mysql_query($query);
 
@@ -36,12 +36,24 @@ if (!$rs) {
     echo "Could not execute query: $query\n";
     trigger_error(mysql_error(), E_USER_ERROR);
 } else {
-    echo "Query: $query executed\n"; 
+    //echo "Query: $query executed\n"; 
 }
 
-$row = mysql_fetch_row($rs);
+echo "positions : [ \n";
+$numresults = mysql_num_rows($rs);
+$counter = 0;
+while( $row = mysql_fetch_array($rs) )
+{
+    echo "{ id:row[0], latitude:$row[1], longitude:$row[2], devname:$row[3] }";
+    ++$counter;
+    if( $counter < $numresults )
+    {
+        echo ",";
+    }
+    echo "\n";
+}
 
-echo "$row[0] $row[1]\n";
+echo "] \n";
 
 mysql_close();
 
